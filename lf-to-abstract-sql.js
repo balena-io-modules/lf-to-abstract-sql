11d2f20b5feb2e8dc9cfe8f2b2cce06ec7a2cc4c
Tim Perry <pimterry@gmail.com>
2018-06-01 19:06:38 +0200

    Fix FK field names to use the correct verb as well

--- lf-to-abstract-sql.js
+++ lf-to-abstract-sql.js
@@ -629,17 +629,16 @@
             return factType.join("-");
         },
         LinkTable: function(actualFactType) {
-            var $elf = this, _fromIdx = this.input.idx, binds, query, tableAlias;
+            var $elf = this, _fromIdx = this.input.idx, binds, linkTable, query, tableAlias;
             binds = this._applyWithArgs("RoleBindings", actualFactType);
             tableAlias = this._applyWithArgs("LinkTableAlias", binds, actualFactType);
-            query = [ "SelectQuery", [ "Select", [] ], [ "From", [ this.GetTable(actualFactType).name, tableAlias ] ] ];
+            linkTable = this._applyWithArgs("GetTable", actualFactType);
+            query = [ "SelectQuery", [ "Select", [] ], [ "From", [ linkTable.name, tableAlias ] ] ];
             _.each(binds, function(bind, i) {
-                var baseIdentifierName = actualFactType[2 * i][1], linkTable = $elf.GetTable(actualFactType), targetTable = $elf.GetTable(baseIdentifierName), foreignKeyField;
-                if (!targetTable.primitive) {
-                    foreignKeyField = _.filter(linkTable.fields, function(field) {
-                        return "ForeignKey" === field.dataType && field.references.resourceName === baseIdentifierName;
-                    })[0];
-                    $elf.AddWhereClause(query, [ "Equals", [ "ReferencedField", tableAlias, foreignKeyField.fieldName ], [ "ReferencedField", bind.binding[1], targetTable.idField ] ]);
+                var baseIdentifierName = actualFactType[2 * i][1];
+                if (!$elf.GetTable(baseIdentifierName).primitive) {
+                    var relationshipMapping = $elf.relationships[linkTable.name][baseIdentifierName].$;
+                    $elf.AddWhereClause(query, [ "Equals", [ "ReferencedField", tableAlias, relationshipMapping[0] ], [ "ReferencedField", bind.binding[1], relationshipMapping[1][1] ] ]);
                 }
             });
             return [ "Exists", query ];
