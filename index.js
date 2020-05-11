29aef57618f13597d99f839b4e63f9dab465c303
Pagan Gazzard <page@balena.io>
2020-05-11 15:51:01 +0100

    Rename to @balena/lf-to-abstract-sql
    Change-type: major

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (function (root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
-		define(['@resin/lf-to-abstract-sql/lf-to-abstract-sql-prep', '@resin/lf-to-abstract-sql/lf-to-abstract-sql'], factory);
+		define(['@balena/lf-to-abstract-sql/lf-to-abstract-sql-prep', '@balena/lf-to-abstract-sql/lf-to-abstract-sql'], factory);
 	} else if (typeof exports === 'object') {
 		// Node. Does not work with strict CommonJS, but
 		// only CommonJS-like enviroments that support module.exports,
